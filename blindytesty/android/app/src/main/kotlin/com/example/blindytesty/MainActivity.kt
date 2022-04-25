ff8931ed19d0684a1fa6a7973ff17d913cc0f9ae
Guillaume BOEHM <guillaumeboehm@hotmail.fr>
2022-04-25 20:35:45 +0200

    renamed flutter project

---
+++ blindytesty/android/app/src/main/kotlin/com/example/blindytesty/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.blindytesty
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ blindytesty/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:kplayer/kplayer.dart';
+import './app.dart';
+import 'package:blindytesty/src/services/storage.dart';
+
+void main() async {
+  Player.boot();
+  if (kDebugMode) print('initializing storage');
+  await Storage.initStorage();
+  // await Hive.openBox<String>('platform');
+  // Future<void>(() => Player.network(
+  //         "https://p.scdn.co/mp3-preview/beb71b347ec0ec922780288793a6165a83a90f33?cid=0c9ef2bc7b2a4189b06ed071c7dd0dfd")
+  //     .play());
+  runApp(const App());
+}
