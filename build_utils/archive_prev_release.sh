c2fcbecd9360ce267cebdf299615a558898fe664
Guillaume BOEHM <guillaumeboehm@hotmail.fr>
2022-05-14 13:55:35 +0200

    archive build util fix

--- build_utils/archive_prev_release.sh
+++ build_utils/archive_prev_release.sh
@@ -4,5 +4,5 @@ VARS="GIT_DIR RELEASES_DIR" . build_utils/common.sh
 latest="${GIT_DIR%%/}/${RELEASES_DIR%%/}/latest"
 archives="${GIT_DIR%%/}/${RELEASES_DIR%%/}/archives"
 mkdir -p "$latest" "$archives"
-mv "${latest%%/}/*" "${archives%%/}/"
+mv "${latest%%/}"/* "${archives%%/}/" || echo "No latest version.";
 
