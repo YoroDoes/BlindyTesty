cc76c96441de103734206ff5aa6469708314a299
Guillaume BOEHM <guillaumeboehm@hotmail.fr>
2022-05-14 14:52:41 +0200

    clean build directory on release-it

--- build_utils/flutter_doctor.sh
+++ build_utils/flutter_doctor.sh
@@ -9,6 +9,9 @@ cd $FLUTTER_DIR || error "No directory $FLUTTER_DIR";
 #crashes on wrong flutter version
 flutter channel || exit 1;
 flutter doctor -v || exit 1;
+echo "build clean" >&2
+rm -rf "$FLUTTER_DIR/build/"
 # windows update and check
+echo "windows flutter upgrade" >&2;
 ssh -o BatchMode=true windows_server "flutter upgrade --force;" || exit 1;
 # TODO mac update and check
