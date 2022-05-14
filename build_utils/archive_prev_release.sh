24b1fb0b084359d451311ca8b518ebe7af49b534
Guillaume BOEHM <guillaumeboehm@hotmail.fr>
2022-05-14 17:22:03 +0200

    exclude build/web from cleanup / fix windows zip name

--- build_utils/archive_prev_release.sh
+++ build_utils/archive_prev_release.sh
@@ -2,7 +2,6 @@
 VARS="GIT_DIR RELEASES_DIR" . build_utils/common.sh
 
 latest="${GIT_DIR%%/}/${RELEASES_DIR%%/}/latest"
-# archives="${GIT_DIR%%/}/${RELEASES_DIR%%/}/archives"
 archives="${GIT_DIR%%/}/${RELEASES_DIR%%/}/archives/$VERSION"
 mkdir -p "$latest" "$archives"
 mv "${latest%%/}"/* "${archives%%/}/" || echo "No latest version.";
--- build_utils/windows.sh
+++ build_utils/windows.sh
@@ -1,7 +1,5 @@
 VARS="GIT_DIR FLUTTER_DIR RELEASES_DIR PROJECT_NAME WINDOWS_TMP_DIR" . build_utils/common.sh
 
-# cd $FLUTTER_DIR || error "No directory $FLUTTER_DIR";
-
 scp -Bqr "$GIT_DIR/build_utils/windows_clean_build.ps1" windows_server: 2>&-; echo "cleanup copy done";
 ssh -o BatchMode=true windows_server '~/windows_clean_build.ps1; rm -Recurse -ErrorAction Ignore ~/windows_clean_build.ps1;' || exit 1;
 rm -rf "$WINDOWS_TMP_DIR"
@@ -12,4 +10,4 @@ cp -r --parents "$REL_GIT_DIR"build_utils "$REL_FLUTTER_DIR"windows "$REL_FLUTTE
 scp -BCqr "$WINDOWS_TMP_DIR/"* "windows_server:flutter_build/" 2>&-; echo "scp done";
 ssh -o BatchMode=true windows_server "~/flutter_build/build_utils/windows_build.ps1 $PROJECT_NAME $VERSION" || exit 1;
 scp -B "windows_server:flutter_build/release_build.zip" "/tmp/release_build.zip" || exit 1;
-zip -FF "/tmp/release_build.zip" --out "$GIT_DIR/$RELEASES_DIR/latest/$PROJECT_NAME-v$VERSION.zip"
+zip -FF "/tmp/release_build.zip" --out "$GIT_DIR/$RELEASES_DIR/latest/windows-$PROJECT_NAME-v$VERSION.zip"
