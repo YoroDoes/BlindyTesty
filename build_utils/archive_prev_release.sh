7a347612cb86e3e56fcc930e1dd49dcd2fa5e0f8
Guillaume BOEHM <guillaumeboehm@hotmail.fr>
2022-05-14 15:40:46 +0200

    some more prev version attempts

--- build_utils/archive_prev_release.sh
+++ build_utils/archive_prev_release.sh
@@ -2,7 +2,8 @@ set -- 0.0.0 #stub
 VARS="GIT_DIR RELEASES_DIR" . build_utils/common.sh
 
 latest="${GIT_DIR%%/}/${RELEASES_DIR%%/}/latest"
-archives="${GIT_DIR%%/}/${RELEASES_DIR%%/}/archives"
+# archives="${GIT_DIR%%/}/${RELEASES_DIR%%/}/archives"
+archives="${GIT_DIR%%/}/${RELEASES_DIR%%/}/archives/$PREV_VERSION"
 # archives="${GIT_DIR%%/}/${RELEASES_DIR%%/}/archives/$VERSION"
 mkdir -p "$latest" "$archives"
 mv "${latest%%/}"/* "${archives%%/}/" || echo "No latest version.";
