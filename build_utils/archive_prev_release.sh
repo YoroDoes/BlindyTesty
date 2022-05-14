804f75ec31883159e356254c507975d33307085d
Guillaume BOEHM <guillaumeboehm@hotmail.fr>
2022-05-14 15:24:57 +0200

    can't get the previous release version

--- build_utils/archive_prev_release.sh
+++ build_utils/archive_prev_release.sh
@@ -1,8 +1,9 @@
-# set -- 0.0.0 #stub
+set -- 0.0.0 #stub
 VARS="GIT_DIR RELEASES_DIR" . build_utils/common.sh
 
 latest="${GIT_DIR%%/}/${RELEASES_DIR%%/}/latest"
-archives="${GIT_DIR%%/}/${RELEASES_DIR%%/}/archives/$VERSION"
+archives="${GIT_DIR%%/}/${RELEASES_DIR%%/}/archives"
+# archives="${GIT_DIR%%/}/${RELEASES_DIR%%/}/archives/$VERSION"
 mkdir -p "$latest" "$archives"
 mv "${latest%%/}"/* "${archives%%/}/" || echo "No latest version.";
 
