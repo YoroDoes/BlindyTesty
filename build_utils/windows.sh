2d83bbc78edfe27d73b92055fdb8d6d105b2ddc4
Guillaume BOEHM <guillaumeboehm@hotmail.fr>
2022-05-14 15:12:40 +0200

    windows tmp dir fix

--- build_utils/windows.sh
+++ build_utils/windows.sh
@@ -6,7 +6,9 @@ scp -Bqr "$GIT_DIR/build_utils/windows_clean_build.ps1" windows_server: 2>&-; ec
 ssh -o BatchMode=true windows_server '~/windows_clean_build.ps1; rm -Recurse -ErrorAction Ignore ~/windows_clean_build.ps1;' || exit 1;
 rm -rf "$WINDOWS_TMP_DIR"
 mkdir -p "$WINDOWS_TMP_DIR"
-cp -r --parents "$GIT_DIR/build_utils/" "$FLUTTER_DIR/windows" "$FLUTTER_FIR/lib" "$FLUTTER_DIR/assets" "$FLUTTER_DIR/analysis_options.yaml" "$FLUTTER_DIR/blindytesty.iml" "$FLUTTER_DIR/packages" "$FLUTTER_DIR/pubspec.yaml" "$FLUTTER_DIR/test" "$WINDOWS_TMP_DIR" || exit 1;
+REL_GIT_DIR=$(echo "$GIT_DIR" | sed "s|^$PWD/||")
+REL_FLUTTER_DIR=$(echo "$FLUTTER_DIR" | sed "s|^$PWD/||")
+cp -r --parents "$REL_GIT_DIR/build_utils/" "$REL_FLUTTER_DIR/windows" "$REL_FLUTTER_FIR/lib" "$REL_FLUTTER_DIR/assets" "$REL_FLUTTER_DIR/analysis_options.yaml" "$REL_FLUTTER_DIR/blindytesty.iml" "$REL_FLUTTER_DIR/packages" "$REL_FLUTTER_DIR/pubspec.yaml" "$REL_FLUTTER_DIR/test" "$WINDOWS_TMP_DIR" || exit 1;
 scp -BCqr "$WINDOWS_TMP_DIR/"* "windows_server:flutter_build/" 2>&-; echo "scp done";
 ssh -o BatchMode=true windows_server "~/flutter_build/build_utils/windows_build.ps1 $PROJECT_NAME $VERSION" || exit 1;
 scp -B "windows_server:flutter_build/release_build.zip" "/tmp/release_build.zip" || exit 1;
